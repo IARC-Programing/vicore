85f2ed0172469d3ad20b47a51c79d9ecfd5ee488
wattana <103913703+iyou25@users.noreply.github.com>
2023-04-28 14:32:48 +0700

    แ

--- frontend/src/componant/ProjectPage/DeleteProject.jsx
+++ frontend/src/componant/ProjectPage/DeleteProject.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 function handleDeleteClick(projectId) {
-  const confirmed = window.confirm("Confirm Delete Project");
+  const confirmed = window.confirm("คุณต้องการลบโปรเจ็กต์นี้ใช่หรือไม่ ?");
   if (confirmed) {
-    axios.delete(`http://127.0.0.1:5000/projects/delete/${projectId}`)
-      .then(response => {
-        console.log("Project successfully deleted");
-        window.location.href = "/Project";
-        // ทำการอัพเดท UI หรือโหลดหน้าใหม่เพื่อแสดงรายการโปรเจกต์ที่เหลืออยู่
+    axios
+      .delete(`http://127.0.0.1:5000/projects/delete/${projectId}`)
+      .then(function (response) {
+        Swal.fire({
+          title: "Success!",
+          text: "Your delete project successfully",
+          icon: "success",
+          confirmButtonText: "OK"
+        }).then(() => {
+          window.location.href = "/Project";
+        });
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Failed to delete project", error);
         // แสดงข้อผิดพลาดที่ console หรือแจ้งเตือนผู้ใช้งาน
       });
